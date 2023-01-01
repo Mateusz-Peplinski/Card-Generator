59d2cb135d690c388ccb07ab3853286859f4ba89
Mateusz <74623154+Mateusz-Peplinski@users.noreply.github.com>
2023-01-01 21:54:35 +0000

    Add files via upload

---
+++ src/main.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Card_Generator
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.genButton = new System.Windows.Forms.Button();
+            this.cardNumberTextbox = new System.Windows.Forms.TextBox();
+            this.expDatTextBox = new System.Windows.Forms.TextBox();
+            this.securityNumberTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // genButton
+            // 
+            this.genButton.BackColor = System.Drawing.Color.Orange;
+            this.genButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.genButton.FlatAppearance.BorderSize = 0;
+            this.genButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.genButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genButton.ForeColor = System.Drawing.Color.Black;
+            this.genButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.genButton.Location = new System.Drawing.Point(49, 326);
+            this.genButton.Name = "genButton";
+            this.genButton.Size = new System.Drawing.Size(264, 80);
+            this.genButton.TabIndex = 0;
+            this.genButton.Text = "Generate";
+            this.genButton.UseVisualStyleBackColor = false;
+            this.genButton.Click += new System.EventHandler(this.genButton_Click);
+            // 
+            // cardNumberTextbox
+            // 
+            this.cardNumberTextbox.BackColor = System.Drawing.Color.Black;
+            this.cardNumberTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.cardNumberTextbox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.cardNumberTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardNumberTextbox.ForeColor = System.Drawing.SystemColors.Window;
+            this.cardNumberTextbox.Location = new System.Drawing.Point(91, 237);
+            this.cardNumberTextbox.Name = "cardNumberTextbox";
+            this.cardNumberTextbox.ReadOnly = true;
+            this.cardNumberTextbox.Size = new System.Drawing.Size(571, 54);
+            this.cardNumberTextbox.TabIndex = 1;
+            this.cardNumberTextbox.Text = "4917 3000 0000 0008";
+            // 
+            // expDatTextBox
+            // 
+            this.expDatTextBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.expDatTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.expDatTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.expDatTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.expDatTextBox.ForeColor = System.Drawing.Color.White;
+            this.expDatTextBox.Location = new System.Drawing.Point(411, 311);
+            this.expDatTextBox.Name = "expDatTextBox";
+            this.expDatTextBox.ReadOnly = true;
+            this.expDatTextBox.Size = new System.Drawing.Size(152, 46);
+            this.expDatTextBox.TabIndex = 2;
+            this.expDatTextBox.Text = "XX/XX";
+            // 
+            // securityNumberTextBox
+            // 
+            this.securityNumberTextBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.securityNumberTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.securityNumberTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.securityNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.securityNumberTextBox.ForeColor = System.Drawing.Color.White;
+            this.securityNumberTextBox.Location = new System.Drawing.Point(411, 381);
+            this.securityNumberTextBox.Name = "securityNumberTextBox";
+            this.securityNumberTextBox.ReadOnly = true;
+            this.securityNumberTextBox.Size = new System.Drawing.Size(152, 46);
+            this.securityNumberTextBox.TabIndex = 3;
+            this.securityNumberTextBox.Text = "XXX";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::Card_Generator.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(801, 463);
+            this.Controls.Add(this.securityNumberTextBox);
+            this.Controls.Add(this.expDatTextBox);
+            this.Controls.Add(this.cardNumberTextbox);
+            this.Controls.Add(this.genButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main";
+            this.Text = "Card Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button genButton;
+        private System.Windows.Forms.TextBox cardNumberTextbox;
+        private System.Windows.Forms.TextBox expDatTextBox;
+        private System.Windows.Forms.TextBox securityNumberTextBox;
+    }
+}
+
---
+++ src/main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Generator
+{
+    public partial class main : Form
+    {
+        private Random RandomNumberGen;
+        public main()
+        {
+            InitializeComponent();
+            RandomNumberGen = new Random();
+            cardNumberTextbox.Text = "";
+            expDatTextBox.Text = "";
+            securityNumberTextBox.Text = "";
+        }
+
+
+        private String generateCardNumber()
+        {
+            string cardNumber = "";
+            
+            
+            int length = RandomNumberGen.Next(16,19);
+
+            for (int i=0; i < length; i++)
+            {
+                int nextNumber = RandomNumberGen.Next(0,9);
+                if(i == 4 || i == 8 || i == 12)
+                {
+                    cardNumber += " ";
+                }
+                cardNumber += nextNumber.ToString();
+            }
+
+            return cardNumber;
+        }
+
+        private String generateExpireDate()
+        {
+            string expDate = "";
+
+            int month = RandomNumberGen.Next(1, 12);
+            expDate += month.ToString();
+            expDate += "/";
+            int year = RandomNumberGen.Next(22, 30);
+            expDate += year.ToString();
+            
+            return expDate;
+        }
+
+        private String generateSecurityNumber()
+        {
+            string securityNumber = "";
+
+            for (int i = 0; i < 3; i++) {
+                int num = RandomNumberGen.Next(0,9);
+                securityNumber += num.ToString();
+            }
+
+            return securityNumber;
+        }
+
+        private void genButton_Click(object sender, EventArgs e)
+        {
+            cardNumberTextbox.Text = "";
+            expDatTextBox.Text = "";
+            securityNumberTextBox.Text = "";
+            string cardNumber = generateCardNumber();
+            string expireDate = generateExpireDate();
+            string securityNumber  = generateSecurityNumber();
+            expDatTextBox.Text += expireDate;
+            securityNumberTextBox.Text += securityNumber;
+            cardNumberTextbox.Text += cardNumber;
+        }
+    }
+}
